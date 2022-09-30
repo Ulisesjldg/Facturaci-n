892b872e8b3331ea632df4ebbbb72b0e4037e779
ulisesjldg <ulisesjldg@gmail.com>
2022-09-29 21:59:54 -0500

    First Commit

---
+++ js/controller.js
@@ -0,0 +1,9 @@
+console.log("Archivo Javascript");
+
+function fecha(){
+    var f = new Date();
+    document.write("Fecha: "+f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
+}
+function DigitarCliente(){
+    console.log("Presiono el boton Registrar Cliente");
+}
